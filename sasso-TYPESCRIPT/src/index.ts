901d476f6c91bc8662e56dea152c8617d2211532
Katia Farnataro <kfarnataro@tecnetdatitorino.onmicrosoft.com>
2025-02-27 14:27:57 +0100

    agg

--- sasso-TYPESCRIPT/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as readline from 'readline';
-import { sassoCartaForbici } from './game/logica';
-// per usare readline tipizzato ho installato ==> @types/node
-const input: readline.Interface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-console.log("AVVIO GIOCO ...");
-sassoCartaForbici(input);
